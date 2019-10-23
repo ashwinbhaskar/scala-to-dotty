c677da080ae9e19721b5cab8746cd89b7d92f979
Martin Mauch <martin.mauch@gmail.com>
2019-10-23 13:46:24 +0200

    Fix typos

--- src/main/scala/dotty/core/DependentFunctionTypesDemo.scala
+++ src/main/scala/dotty/core/DependentFunctionTypesDemo.scala
@@ -6,13 +6,13 @@ trait Foo{
   val baz : Baz
 }
 
-//This is dependant method type. This works in Scala 2 as well
+// This is dependent method type. This works in Scala 2 as well
 def extractBaz(f : Foo) : f.Baz = f.baz
 
-//But what if you wanted a function type for extractBax?
-//It was not possible to do that in Scala 2
-//But with dependant function types introduced in dotty, this is possible
-//Read more about dependant function types here
-//https://dotty.epfl.ch/docs/reference/new-types/dependent-function-types.html
+// But what if you wanted a function type for extractBaz?
+// It was not possible to do that in Scala 2.
+// But with dependent function types introduced in dotty, this is possible.
+// Read more about dependent function types here
+// https://dotty.epfl.ch/docs/reference/new-types/dependent-function-types.html
 
 val bazExtractor : (f : Foo) => f.Baz = extractBaz
