d89259a04525d7b1aa96070c281b60e798b75fc6
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2019-11-15 10:22:12 +0530

    Add differences from with

--- src/main/scala/dotty/core/IntersectionTypesDemo.scala
+++ src/main/scala/dotty/core/IntersectionTypesDemo.scala
@@ -1,5 +1,9 @@
 import scala.concurrent.Future
 
+/*
+`&` is similar to `with` in scala 2 when only used as class composition
+*/
+
 trait RedisClient
     def increment(key: String): Future[Unit]
 
@@ -11,12 +15,27 @@ trait CustomerInfoClient
     type Age = Int
     type Gender = String
     def getDetails(customerId: String): Future[(Name, Age, Gender)]
-
-
-
 /*
  You can pass along `dependeny` to all the handlers. They will be able to accpe them as
  `CustomerInfoClient` or `KafkaClient` or `RedisClient`. This example is inspired from 
  John De Goes's article on the module pattern - http://degoes.net/articles/zio-environment#the-module-pattern
 */
 def startServer(dependency: RedisClient & KafkaClient & CustomerInfoClient) = ???
+
+
+/*
+It's different from with in Scala 2 in the sense that `&` is commutative. Unlike with `with` in scala 2,
+both (???: A & B).foo and (???: B & A).foo return type Int
+*/
+
+trait Base
+    def foo: Any
+trait A extends Base
+    override def foo: Int = 1
+trait B extends Base
+    override def foo: Any = "foo"
+
+def func(ab: A & B): Int = ab.foo //returns Int
+
+def func(ba: B & A): Int = ba.foo //return Int
+
