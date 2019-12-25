cceefe8eccee449a1b0562ad80412a7c0486bbbe
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2019-12-25 17:50:56 +0530

    Add check for explicit nulls since we are comiling scala2 code also with dotty

--- src/main/scala/scala2/core/implicits/Conversions.scala
+++ src/main/scala/scala2/core/implicits/Conversions.scala
@@ -1,7 +1,13 @@
 package scala2.core.implicits
 import scala.language.implicitConversions
 
+/*
+The explicit null check is only done because we are compiling all the code with dotty with -Yexplicit-nulls flag enabled
+*/
 object Conversions {
   implicit def int2Integer(i: Int):java.lang.Integer = 
-    java.lang.Integer.valueOf(i)
+    java.lang.Integer.valueOf(i) match {
+      case a: Integer => a
+      case null => throw new Exception("Cannot convert null to integer value")
+    }
 }
